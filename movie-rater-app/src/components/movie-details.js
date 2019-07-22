99e6243446dec3a3393555d3bd459bb340de9714
Brandon <temple747@gmail.com>
2019-07-21 19:06:18 -0500

    added stars to movie details

--- movie-rater-app/src/components/movie-details.js
+++ movie-rater-app/src/components/movie-details.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+var FontAwesome = require('react-fontawesome');
 
 class MovieDetails extends Component {
   render() {
+    const movie = this.props.movie;
     return (
       <React.Fragment>
-        { this.props.movie ? (
+        { movie ? (
           <div>
-            <h3>{this.props.movie.title}</h3>
-            <p>{this.props.movie.description}</p>
+            <h3>{movie.title}</h3>
+            <FontAwesome name="star" className={movie.avg_rating > 0 ? 'yellow' : ''} />
+            <FontAwesome name="star" className={movie.avg_rating > 1 ? 'yellow' : ''} />
+            <FontAwesome name="star" className={movie.avg_rating > 2 ? 'yellow' : ''} />
+            <FontAwesome name="star" className={movie.avg_rating > 3 ? 'yellow' : ''} />
+            <FontAwesome name="star" className={movie.avg_rating > 4 ? 'yellow' : ''} />
+            ({movie.number_of_ratings})
+
+            <p>{movie.description}</p>
           </div>
         ) : null
         }
