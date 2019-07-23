4d39a9c960dbe8978fbbe1133da1c830dd5913cf
Brandon <temple747@gmail.com>
2019-07-22 19:46:27 -0500

    created basic login component, added routing

--- movie-rater-app/src/index.js
+++ movie-rater-app/src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Login from './components/login';
 import './index.css';
+import { Route, BrowserRouter } from 'react-router-dom';
+
+const routing = (
+  <BrowserRouter>
+    <div>
+      <Route exact path="/" component={Login} />
+      <Route exact path="/movies" component={App} />
+    </div>
+  </BrowserRouter>
+)
 
 ReactDOM.render(
-  <App />,
+  routing,
   document.getElementById('root')
 );
